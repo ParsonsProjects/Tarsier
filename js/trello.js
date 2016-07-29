536a98a2862386c5779ff2d4182ff2756e9a75e2
Alan Parsons <alan.parsons@gforces.co.uk>
2016-07-29 16:58:00 +0100

    Corrected functionality of sorting actions into tabs, works when actions are update by trello.

--- js/trello.js
+++ js/trello.js
@@ -14,27 +14,148 @@ $(() => {
 		});
 
 		// check if popup already open
-		if(window.location.href.match(/(trello.com\/c)/g)) checkCardOpen();
+		if(window.location.href.match(/(trello.com\/c)/g)) cardOpen();
 
 	}, 500);
 
+	$('.window').addClass('full-width');
+
 	$('.list-card').not('.js-open-quick-card-editor').on('click', () => {
-		checkCardOpen();
+		cardOpen();
 	});
+
 
 
 });
 
-const checkCardOpen = () => {
+const cardOpen = () => {
 
-	let $cardWindow = $('.window-module.other-actions .u-clearfix');
+	addActions();
+	sortActions();
+
+}
+
+const sortActions = () => {
+
+	let tabbedNav = '<div class="tabbed-pane-nav"><ul style="text-align: left;">';
+	tabbedNav += '<li class="tabbed-pane-nav-item"><a class="tabbed-pane-nav-item-button active js-all">All</a></li>';
+	tabbedNav += '<li class="tabbed-pane-nav-item"><a class="tabbed-pane-nav-item-button js-comments">Comments</a></li>';
+	tabbedNav += '<li class="tabbed-pane-nav-item"><a class="tabbed-pane-nav-item-button js-other">Activity</a></li>';
+	tabbedNav += '</ul></div>';
+
+	let tabbedContent = '<div class="tabbed-panes">';
+	tabbedContent += '<div class="tab-pane js-tab-all"></div>';
+	tabbedContent += '<div style="display: none;" class="tab-pane js-tab-comments"></div>';
+	tabbedContent += '<div style="display: none;" class="tab-pane js-tab-other"></div>';
+
+	let allActions = [];
+	let commentActions = [];
+	let otherActions = [];
+
+	$('.js-list-actions').before(tabbedNav + tabbedContent);
 
 	let cardInterval = setInterval(() => {
 
-		if($cardWindow.length) {
+		if(!$('.js-loading-card-actions').is(':visible')) {
 
-			addActions($cardWindow);
-			sortActions();
+			init();
+
+			function init() {
+				loopActions();
+				showAll();
+				startObserver();
+			}
+
+			function loopActions() {
+
+				allActions = [];
+				commentActions = [];
+				otherActions = [];
+				let $actions = $('.js-list-actions > .phenom');
+				$actions.each(function(e) {
+
+					if($(this).hasClass('mod-comment-type')) {
+						commentActions.push($(this));
+					} else {
+						otherActions.push($(this));
+					}
+
+					allActions.push($(this));
+
+				});
+
+			}
+
+			function startObserver() {
+
+				//http://www.skysteve.com/dev/2014/04/01/detecting_dom_changes.html
+				//setup our callback function to loop through each mutation and print the number of nodes
+				//that have been added and removed
+				var fnCallback = function (mutations) {
+				 	observer.disconnect();
+				 	init();
+				};
+
+				//now create our observer and get our target element
+				var observer = new MutationObserver(fnCallback),
+				elTarget = document.querySelector(".js-list-actions"),
+				objConfig = {
+				 	childList: true,
+				 	subtree : true,
+				 	attributes: false,
+				 	characterData : false
+				};
+
+				//then actually do some observing
+				observer.observe(elTarget, objConfig);
+
+			}
+
+			function showAll() {
+				for (var i = 0; i < allActions.length; i++) {
+					allActions[i].appendTo($('.js-tab-all'))
+				}
+			}
+
+			function showComments() {
+				for (var i = 0; i < commentActions.length; i++) {
+					commentActions[i].appendTo($('.js-tab-comments'))
+				}
+			}
+
+			function showOther() {
+				for (var i = 0; i < otherActions.length; i++) {
+					otherActions[i].appendTo($('.js-tab-other'))
+				}
+			}
+
+			$('.js-all').on('click', (e) => {
+				showAll();
+				$('.js-list-actions').hide();
+				$('.tabbed-pane-nav-item-button').removeClass('active');
+				$('.js-all').addClass('active');
+				$('.tab-pane').hide();
+				$('.js-tab-all').show();
+			});
+
+			$('.js-comments').on('click', (e) => {
+				showComments();
+				$('.js-list-actions').hide();
+				$('.tabbed-pane-nav-item-button').removeClass('active');
+				$('.js-comments').addClass('active');
+				$('.tab-pane').hide();
+				$('.js-tab-comments').show();
+			});
+
+			$('.js-other').on('click', (e) => {
+				showOther();
+				$('.js-list-actions').hide();
+				$('.tabbed-pane-nav-item-button').removeClass('active');
+				$('.js-other').addClass('active');
+				$('.tab-pane').hide();
+				$('.js-tab-other').show();
+			});
+
 			clearInterval(cardInterval);
 
 		}
@@ -43,133 +164,76 @@ const checkCardOpen = () => {
 
 }
 
-const sortActions = () => {
+const addActions = () => {
 
-	var tabbedNav = '<div class="tabbed-pane-nav"><ul>';
-	tabbedNav += '<li class="tabbed-pane-nav-item"><a class="tabbed-pane-nav-item-button active js-all">All</a></li>';
-	tabbedNav += '<li class="tabbed-pane-nav-item"><a class="tabbed-pane-nav-item-button js-comments">Comments</a></li>';
-	tabbedNav += '<li class="tabbed-pane-nav-item"><a class="tabbed-pane-nav-item-button js-other">Other</a></li>';
-	tabbedNav += '</ul></div>';
+	let $cardWindow = $('.window-module.other-actions .u-clearfix');
 
-	var tabbedContent = '<div class="tabbed-panes">';
-	tabbedContent += '<div class="tab-pane js-tab-all"></div>';
-	tabbedContent += '<div style="display: none;" class="tab-pane js-tab-comments"></div>';
-	tabbedContent += '<div style="display: none;" class="tab-pane js-tab-other"></div>';
+	let cardInterval = setInterval(() => {
 
-	var allActions = [];
-	var commentActions = [];
-	var otherActions = [];
+		if($cardWindow.length) {
 
-	$('.js-list-actions').before(tabbedNav + tabbedContent);
-	$('.js-list-actions').hide();
+			let $startTimerBtn = $('<a class="button-link js-start-timer" href="#"><span class="icon-sm icon-clock"></span> Start Timer</a>').prependTo($cardWindow);
+			let $pauseTimerBtn = $('<a class="button-link js-pause-timer hide" href="#"><span class="icon-sm icon-clock"></span> Pause Timer</a>').prependTo($cardWindow);
+			let $stopTimerBtn = $('<a class="button-link js-stop-timer hide" href="#"><span class="icon-sm icon-close"></span> Stop Timer</a>').prependTo($cardWindow);
 
-	// @todo add interval to show new actions comments/change etc
-	// total length of items
-	// if it changes add to array
+			$startTimerBtn.on('click', (e) => {
+				e.preventDefault();
 
-	$('.js-list-actions > div').each(function(e) {
+				timerID = '';
+				cardID = '';
 
-		if($(this).hasClass('mod-comment-type')) {
-			commentActions.push($(this));
-		} else {
-			otherActions.push($(this));
-		}
+				$.get(window.location.href + '.json', (data) => {
+					cardID = data.id;
+					timerStarted = true;
+					chrome.runtime.sendMessage({
+				    	from: 'trello',
+					    subject: 'set',
+					    label: 'timerStarted',
+					    value: true
+					});
+					timerStart('Timer started - *'+moment().format('H:mm a on MMM D, YYYY')+'*', {'type': 'play', 'time': moment()});
+					$startTimerBtn.addClass('hide');
+					$pauseTimerBtn.removeClass('hide');
+					$stopTimerBtn.removeClass('hide');
+				});
 
-		allActions.push($(this));
-		$(this).appendTo($('.js-tab-all'));
-
-	});
-
-	$('body').on('click', '.js-all', (e) => {
-		for (var i = 0; i < allActions.length; i++) {
-			allActions[i].appendTo($('.js-tab-all'))
-		}
-		$('.tabbed-pane-nav-item-button').removeClass('active');
-		$('.js-all').addClass('active');
-		$('.tab-pane').hide();
-		$('.js-tab-all').show();
-	});
-
-	$('body').on('click', '.js-comments', (e) => {
-		for (var i = 0; i < commentActions.length; i++) {
-			commentActions[i].appendTo($('.js-tab-comments'));
-		}
-		$('.tabbed-pane-nav-item-button').removeClass('active');
-		$('.js-comments').addClass('active');
-		$('.tab-pane').hide();
-		$('.js-tab-comments').show();
-	});
-
-	$('body').on('click', '.js-other', (e) => {
-		for (var i = 0; i < otherActions.length; i++) {
-			otherActions[i].appendTo($('.js-tab-other'));
-		}
-		$('.tabbed-pane-nav-item-button').removeClass('active');
-		$('.js-other').addClass('active');
-		$('.tab-pane').hide();
-		$('.js-tab-other').show();
-	});
-
-}
-
-const addActions = ($cardWindow) => {
-
-	let $startTimerBtn = $('<a class="button-link js-start-timer" href="#"><span class="icon-sm icon-clock"></span> Start Timer</a>').prependTo($cardWindow);
-	let $pauseTimerBtn = $('<a class="button-link js-pause-timer hide" href="#"><span class="icon-sm icon-clock"></span> Pause Timer</a>').prependTo($cardWindow);
-	let $stopTimerBtn = $('<a class="button-link js-stop-timer hide" href="#"><span class="icon-sm icon-close"></span> Stop Timer</a>').prependTo($cardWindow);
-
-	$startTimerBtn.on('click', (e) => {
-		e.preventDefault();
-
-		timerID = '';
-		cardID = '';
-
-		$.get(window.location.href + '.json', (data) => {
-			cardID = data.id;
-			timerStarted = true;
-			chrome.runtime.sendMessage({
-		    	from: 'trello',
-			    subject: 'set',
-			    label: 'timerStarted',
-			    value: true
 			});
-			timerStart('Timer started - *'+moment().format('H:mm a on MMM D, YYYY')+'*', {'type': 'play', 'time': moment()});
-			$startTimerBtn.addClass('hide');
-			$pauseTimerBtn.removeClass('hide');
-			$stopTimerBtn.removeClass('hide');
-		});
 
-	});
+			$pauseTimerBtn.on('click', (e) => {
+				e.preventDefault();
 
-	$pauseTimerBtn.on('click', (e) => {
-		e.preventDefault();
+				chrome.runtime.sendMessage({
+			    	from: 'trello',
+				    subject: 'set',
+				    label: 'timerPaused',
+				    value: true
+				});
+				timerLog('Timer paused - *'+moment().format('H:mm a on MMM D, YYYY')+'*', {'type': 'paused', 'time': moment()});
 
-		chrome.runtime.sendMessage({
-	    	from: 'trello',
-		    subject: 'set',
-		    label: 'timerPaused',
-		    value: true
-		});
-		timerLog('Timer paused - *'+moment().format('H:mm a on MMM D, YYYY')+'*', {'type': 'paused', 'time': moment()});
+			});
 
-	});
+			$stopTimerBtn.on('click', (e) => {
+				e.preventDefault();
 
-	$stopTimerBtn.on('click', (e) => {
-		e.preventDefault();
+				timerStarted = false;
+				chrome.runtime.sendMessage({
+			    	from: 'trello',
+				    subject: 'set',
+				    label: 'timerStarted',
+				    value: false
+				});
+				timerLog('Timer stopped - *'+moment().format('H:mm a on MMM D, YYYY')+'*', {'type': 'stopped', 'time': moment()});
+				$stopTimerBtn.addClass('hide');
+				$pauseTimerBtn.addClass('hide');
+				$startTimerBtn.removeClass('hide');
 
-		timerStarted = false;
-		chrome.runtime.sendMessage({
-	    	from: 'trello',
-		    subject: 'set',
-		    label: 'timerStarted',
-		    value: false
-		});
-		timerLog('Timer stopped - *'+moment().format('H:mm a on MMM D, YYYY')+'*', {'type': 'stopped', 'time': moment()});
-		$stopTimerBtn.addClass('hide');
-		$pauseTimerBtn.addClass('hide');
-		$startTimerBtn.removeClass('hide');
+			});
 
-	});
+			clearInterval(cardInterval);
+
+		}
+
+	}, 100);
 
 }
 
